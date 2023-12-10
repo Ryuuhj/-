39ebe2e88d08c4f25ca37930d4e64f60c190d975
glennni <97296543+Ryuuhj@users.noreply.github.com>
2023-12-10 20:23:26 +0900

    [level 3] Title: 베스트앨범, Time: 7.78 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,65 +1,44 @@
 import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
 class Solution {
-    static HashMap<Integer, List<Music>> musicByGenre = new HashMap<>(); //재생횟수를 장르 고유 인덱스로 사용
-    static Map<String, Integer> genreCnt;
-    public int[] solution(String[] genres, int[] plays) {
-        List<Integer> ans = new ArrayList<>();
-        genreCnt = countGenrePlay(genres, plays);
-        makeMusicList(genres, plays);
-        //총 재생 횟수 내림차순 정렬
-        List<Integer> cntList = new ArrayList<>(genreCnt.values());
-        cntList.sort(Collections.reverseOrder());
-        for (int key : cntList) {
-            if(musicByGenre.get(key).size() == 1) {
-                ans.add(musicByGenre.get(key).get(0).idx); continue;
-            }
-            Collections.sort(musicByGenre.get(key));
-            musicByGenre.get(key).subList(0, 2).forEach(m -> ans.add(m.idx));
+   public int[] solution(String[] genres, int[] plays) {
+        return IntStream.range(0, genres.length)
+                .mapToObj(i -> new Music(i, plays[i], genres[i]))
+                .collect(Collectors.groupingBy(Music::getGenre))
+                .entrySet().stream() //genre같은 것 끼리 Map 생성 후 반환
+                .sorted((a, b) -> sum(b.getValue()) - sum(a.getValue()))
+                .flatMap(x -> x.getValue().stream().sorted().limit(2))
+                .mapToInt(x -> x.id).toArray();
+    }
+    
+    private int sum(List<Music> value) {
+        int answer = 0;
+        for (Music music : value) {
+            answer += music.played;
         }
-        return ans.stream().mapToInt(i -> i).toArray();
+        return answer;
     }
 
-    private void makeMusicList(String[] genres, int[] plays) {
-        for (int i = 0; i < plays.length; i++) {
-            int totalCnt = genreCnt.get(genres[i]);
-            if (!musicByGenre.containsKey(totalCnt)) {
-                musicByGenre.put(totalCnt, new ArrayList<>());
-            }
-            musicByGenre.get(totalCnt).add(new Music(i, plays[i]));
-        }
-    }
+    public class Music implements Comparable<Music> {
+        private int played;
+        private int id;
+        private String genre;
 
-    private Map<String, Integer> countGenrePlay(String[] genres, int[] plays) {
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < genres.length; i++) {
-            if (!map.containsKey(genres[i])) {
-                map.put(genres[i], plays[i]);
-                continue;
-            }
-            map.put(genres[i], map.get(genres[i]) + plays[i]);
-        }
-        return map;
-    }
-
-    class Music implements Comparable<Music> {
-        int idx;
-        int playCnt;
-
-        public Music(int idx, int playCnt) {
-            this.idx = idx;
-
-            this.playCnt = playCnt;
+        public Music(int id, int played, String genre) {
+            this.played = played;
+            this.id = id;
+            this.genre = genre;
         }
 
         @Override
         public int compareTo(Music o) {
-            if(this.playCnt > o.playCnt) return -1;
-            else if(this.playCnt < o.playCnt) return 1;
-            else {
-                if(this.idx < o.idx) return -1;
-                else if(this.idx > o.idx) return 1;
-            }
-            return 0;
+            if(this.played == o.played) return this.id - o.id;
+            return o.played - this.played;
+        }
+        public String getGenre() {
+            return this.genre;
         }
     }
 }
