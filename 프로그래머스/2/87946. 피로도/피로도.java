5b30cb0a09a01dbd6433895cb5aaddd796320fb3
glennni <97296543+Ryuuhj@users.noreply.github.com>
2023-12-19 23:32:44 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,37 +1,23 @@
 class Solution {
-    static int rpMin = Integer.MAX_VALUE;
     static int dgNum, answer;
     static boolean[] visited;
-    static boolean flag = false;
-    static int[][] dg;
     public int solution(int k, int[][] dungeons) {
         answer = 0;
         dgNum = dungeons.length;
         visited = new boolean[dgNum];
-        dg = dungeons.clone();
-        for (int i = 0; i < dgNum; i++) {
-            rpMin = Math.min(rpMin, dungeons[i][0]);
-        }
-        explore(k, 0);
+        explore(k, 0, dungeons);
         return answer;
     }
-    
-    private void explore(int now, int visitCnt) {
-        if(now < 0 || flag) return;
-        if(visitCnt == dgNum){
-            answer = visitCnt;
-            flag = true;
-            return;
-        }else if (now < rpMin){
-            answer = Math.max(answer, visitCnt);
-            return;
-        }
+
+    private void explore(int now, int visitCnt, int[][] dungeons) {
         for (int i = 0; i < dgNum; i++) {
-            if(!visited[i] && dg[i][0] <= now){
+            if(!visited[i] && dungeons[i][0] <= now){
                 visited[i] = true;
-                explore(now - dg[i][1], visitCnt + 1);
+                explore(now - dungeons[i][1], visitCnt + 1, dungeons);
                 visited[i] = false;
             }
         }
+        //가능한 모든 던전에 대한 탐색을 마친 경우, 최댓값 갱신
+        answer = Math.max(answer, visitCnt);
     }
 }
