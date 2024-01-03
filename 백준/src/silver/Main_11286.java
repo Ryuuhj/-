06d9e1e497c87d0de9721c525151706a547d2f4d
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2024-01-03 22:19:27 +0900

    11286

--- 백준/src/silver/Main_11286.java
+++ 백준/src/silver/Main_11286.java
@@ -3,8 +3,6 @@ package silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
