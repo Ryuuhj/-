76d7e13576886c7d0e451bb589b186bf795192e0
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2024-01-14 23:55:24 +0900

    11725

--- 백준/src/silver/Main_11725.java
+++ 백준/src/silver/Main_11725.java
@@ -4,59 +4,54 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_11725 {
     static int N;
     static StringTokenizer st;
-    static Node root = new Node(1);
-    static int[] parent;
+    static ArrayList<Integer>[] tree;
+    static boolean[] visit;
+    static int[] parents;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
-        parent = new int[N + 1];
-        while (N-- > 0) {
+        tree = new ArrayList[N + 1];
+        parents = new int[N + 1];
+        visit = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            tree[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int n1 = Integer.parseInt(st.nextToken());
             int n2 = Integer.parseInt(st.nextToken());
-            insertNode(root, n1, n2);
+            tree[n1].add(n2);
+            tree[n2].add(n1);
         }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < parent.length; i++) {
-            sb.append(parent[i]).append("\n");
+
+        search(1);
+        for (int i = 2; i <= N; i++) {
+            sb.append(parents[i]).append("\n");
         }
         System.out.println(sb);
     }
 
-    private static void insertNode(Node root, int n1, int n2) {
-        if (root.value == n1 || root.value == n2) {
-            int p = 0; int c = 0;
-            if (root.value == n1) {
-                p = n1; c = n2;
-            } else {
-                p = n2; c = n1;
+    private static void search(int root) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        visit[1] = true;
+        while (!queue.isEmpty()) {
+            int parent = queue.poll();
+            for (int child : tree[parent]) {
+                if(visit[child]) continue;
+                parents[child] = parent;
+                queue.add(child);
+                visit[child] = true;
             }
-            root.setChilds(new Node(c));
-            parent[c] = p;
-        } else {
-            for (Node child : root.childs) {
-                insertNode(child, n1, n2);
-            }
-        }
-    }
-
-    private static class Node {
-        int value;
-        ArrayList<Node> childs;
-
-        Node(int value) {
-            this.value = value;
-            this.childs = new ArrayList<>();
-        }
-
-        public void setChilds(Node node) {
-            this.childs.add(node);
         }
     }
 }
