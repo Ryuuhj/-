6722ebd512bf741f905d856221bc6343b54ddd0d
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2024-02-26 22:18:41 +0900

    11055

---
+++ 백준/src/silver/Main_11055.java
@@ -0,0 +1,23 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_11055 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < num; j++) {
+                dp[num] = Math.max(dp[num], dp[j] + num);
+            }
+        }
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
