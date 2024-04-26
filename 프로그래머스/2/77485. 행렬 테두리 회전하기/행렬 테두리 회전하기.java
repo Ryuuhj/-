c5dbda234e5e3be1d3466675af42bbf68ed93614
glennni <97296543+Ryuuhj@users.noreply.github.com>
2024-04-26 16:09:07 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 8.25 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,65 +1,46 @@
 import java.util.*;
 class Solution {
-    static int x, y, nx, ny;
-    static int[][] arr;
-    static int[][] move = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    static Deque<Integer> dq;
-    
+    static int[][] d = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    static int[][] map;
     public int[] solution(int rows, int columns, int[][] queries) {
         int[] answer = new int[queries.length];
-        arr = new int[rows + 1][columns + 1];
-        //초기화
-        int k = 1;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= columns; j++) {
-                arr[i][j] = k;
-                k++;
-            }
-        }
-        dq = new ArrayDeque<>();
-        for (int i = 0; i < queries.length; i++) {
-            answer[i] = spin(queries[i][0], queries[i][1], queries[i][2], queries[i][3]);
+        map = makeMap(rows, columns);
+        for(int i=0; i<queries.length; i++){
+            answer[i] = spinMap(queries[i][0], queries[i][1], queries[i][2], queries[i][3]);
         }
         return answer;
     }
-    public int spin(int sx, int sy, int ex, int ey) {
-        int dir = 0;
-        int min = Integer.MAX_VALUE;
-        x = sx;
-        y = sy;
-        //테두리에 있는 숫자들 덱에 넣음
-        do {
-            dq.addLast(arr[x][y]);
-            nx = x + move[dir][0];
-            ny = y + move[dir][1];
-            if (nx > ex || ny > ey || nx < sx || ny < sy) {
-                dir = (dir + 1) % 4;
-                x += move[dir][0];
-                y += move[dir][1];
-            } else {
-                x = nx;
-                y = ny;
+    
+    private int spinMap(int sr, int sc, int er, int ec){
+        int nowR = sr, nowC = sc;
+        int min = 20000, dir = 0;
+        int pre = map[sr][sc];
+        while(true){
+            int nextR = nowR + d[dir][0];
+            int nextC = nowC + d[dir][1];
+            if(nextR < sr || nextR > er || nextC < sc || nextC > ec) {
+                dir = (dir+1)%4;
+                continue;
             }
-        } while (x != sx || y != sy);
-        //한 칸씩 앞으로 옮김
-        dq.addFirst(dq.pollLast());
-        
-        //이후 다시 처음부터 변경
-        x = sx; y = sy;
-        while (!dq.isEmpty()) {
-            arr[x][y] = dq.pollFirst();
-            min = Math.min(min, arr[x][y]);
-            nx = x + move[dir][0];
-            ny = y + move[dir][1];
-            if (nx > ex || ny > ey || nx < sx || ny < sy) {
-                dir = (dir + 1) % 4;
-                x += move[dir][0];
-                y += move[dir][1];
-            } else {
-                x = nx;
-                y = ny;
-            }
+            min = Math.min(min, pre);
+            int tmp = map[nextR][nextC];
+            map[nextR][nextC] = pre;
+            pre = tmp;
+            nowR = nextR; 
+            nowC = nextC;
+            if(nowR == sr && nowC == sc)
+                break;
         }
         return min;
+    }
+    private int[][] makeMap(int rows, int columns){
+        int[][] map = new int[rows+1][columns+1];
+        int k = 1;
+        for(int i=1; i<=rows; i++){
+            for(int j=1; j<=columns; j++){
+                map[i][j] = k++;
+            }
+        }
+        return map;
     }
 }
