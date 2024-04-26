f8905394ec25efeea753c88326a78167192ec098
glennni <97296543+Ryuuhj@users.noreply.github.com>
2024-04-26 14:56:19 +0900

    [level 2] Title: 구명보트, Time: 8.91 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,18 +2,15 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        int front = 0; int rear = people.length-1;
         Arrays.sort(people);
+        int front = 0, rear = people.length - 1;
         while(front <= rear){
-            answer++;
-            
-            if(front == rear)
-                break;
-        
             if(people[front] + people[rear] <= limit)
-                front++;  
+                front++;
             rear--;
+            answer++;
         }
+        
         return answer;
     }
 }
