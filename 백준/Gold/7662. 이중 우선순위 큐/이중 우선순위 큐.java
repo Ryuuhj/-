d3c64f186224bf6debd6cbe3dcf79a7c11c67278
glennni <97296543+Ryuuhj@users.noreply.github.com>
2024-08-16 00:46:49 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2516 ms, Memory: 347712 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,41 +1,71 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
-    static int T, k;
-    static String cmd;
-    static TreeMap<Integer, Integer> map;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int k, num;
+    static String input;
+    static PriorityQueue<Integer> ascQueue = new PriorityQueue<>();
+    static PriorityQueue<Integer> descQueue = new PriorityQueue<>(Collections.reverseOrder());
+    static Map<Integer, Integer> cnt = new HashMap<>();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        T = Integer.parseInt(br.readLine());
+
         while (T-- > 0) {
-            k = Integer.parseInt(br.readLine());
-            map = new TreeMap<>();
-            while (k-- > 0) {
-                cmd = br.readLine();
-                if(cmd.equals("D -1")){ //최솟값
-                    if(!map.isEmpty()) {
-                        if (map.firstEntry().getValue() == 1)
-                            map.remove(map.firstKey());
-                        else map.put(map.firstKey(), map.firstEntry().getValue() - 1);
-                    }
-                } else if (cmd.equals("D 1")) {
-                    if(!map.isEmpty()) {
-                        if (map.lastEntry().getValue() == 1)
-                            map.remove(map.lastKey());
-                        else map.put(map.lastKey(), map.lastEntry().getValue() - 1);
-                    }
-                }else {
-                    int n = Integer.parseInt(cmd.substring(2));
-                    map.put(n, map.getOrDefault(n, 0) + 1); //없을 경우 1, 있을 경우 +1
-                }
-            }
-            if(map.isEmpty()) sb.append("EMPTY\n");
-            else sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
+            sb.append(executeInst()).append("\n");
         }
+
         System.out.println(sb);
+    }
+
+    private static String executeInst() throws Exception {
+        ascQueue.clear();
+        descQueue.clear();
+        cnt.clear();
+
+        k = Integer.parseInt(br.readLine());
+        while (k-- > 0) {
+            input = br.readLine();
+            //추가
+            if (input.charAt(0) == 'I') {
+                num = Integer.parseInt(input.substring(2));
+                ascQueue.add(num);
+                descQueue.add(num);
+                if (cnt.containsKey(num))
+                    cnt.put(num, cnt.get(num) + 1);
+                else
+                    cnt.put(num, 1);
+            }//삭제
+            else {
+                if (input.charAt(2) == '1')
+                    pollingQueue(descQueue);
+                else
+                    pollingQueue(ascQueue);
+            }
+        }
+        if (cnt.size() == 0)
+            return "EMPTY";
+        
+        List<Integer> leftNumber = new ArrayList<>(cnt.keySet());
+        Collections.sort(leftNumber);
+        return leftNumber.get(leftNumber.size() - 1) + " " + leftNumber.get(0);
+    }
+
+    private static void pollingQueue(PriorityQueue<Integer> pq) {
+        while (!pq.isEmpty()) {
+            num = pq.poll();
+            if (!cnt.containsKey(num))
+                continue;
+
+            if (cnt.get(num) == 1)
+                cnt.remove(num);
+            else
+                cnt.put(num, cnt.get(num) - 1);
+
+            break;
+        }
     }
 }
