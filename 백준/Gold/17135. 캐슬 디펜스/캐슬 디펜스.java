116a18454073ffd86f63dd5f6b562245a8c1acf6
glennni <97296543+Ryuuhj@users.noreply.github.com>
2024-08-17 18:00:19 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 304 ms, Memory: 26660 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -1,131 +1,109 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int[][] map;
-	static int N, M, D, maxKill = 0;
-	static List<int[]> enemies = new ArrayList<>();
-	static int ivy = 0;
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		D = Integer.parseInt(st.nextToken());
-		map = new int[N+1][M];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == 1)
-					enemies.add(new int[] {i, j});
-			}
-		}
-		int[] archerPos = new int[3];
-		setArcher(0, 0, archerPos);
-		System.out.println(maxKill);
-	}
+    static int N, M, D, maxKillCnt = 0;
+    static ArrayList<Pos> enemies = new ArrayList<>();
+    static Pos[] archers = new Pos[3];
 
-	private static void setArcher(int idx, int cnt, int[] archerPos) {
-		if(cnt == 3) {
-			ivy++;
-			maxKill = Math.max(maxKill, playGames(archerPos));
-			return;
-		}
-		for (int i = idx; i < M; i++) {
-			archerPos[cnt] = i;
-			setArcher(idx+1, cnt+1, archerPos);
-		}
-		
-	}
-	private static List<int[]> copy(List<int[]> list) {
-		List<int[]> tmp = new ArrayList<>();
-		for(int[] p : list) {
-			tmp.add(new int[] {p[0], p[1]});
-		}
-		return tmp;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	private static int playGames(int[] archerPos) {
-		int[][] cMap = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				cMap[i][j] = map[i][j];
-			}
-		}
-		//적 위치 복사
-		List<int[]> eList = copy(enemies);
-		int cnt = 0;
-		//archer위치 기준으로 제거
-		while(true) {
-			//적 없으면 멈춤
-			if(eList.isEmpty()) break;
-			Set<int[]> target = new HashSet<>();
-			//궁수 3명 한 명씩 돌아가며 처리
-			for (int l = 0; l < 3; l++) {
-				PriorityQueue<Enemy> pq = new PriorityQueue<Main.Enemy>();
-				boolean[][] kill = new boolean[N][M];
-				int ay = archerPos[l];
-				//가능한 적들 저장
-				for(int[] e : eList) {
-					int dist = Math.abs(N - e[0]) + Math.abs(ay - e[1]);
-					if(dist <= D)
-						pq.add(new Enemy(e[0], e[1], dist));	
-				}
-				//제일 가까운 적 죽임
-				if(!pq.isEmpty()) {
-					Enemy e = pq.poll();
-					if(!kill[e.x][e.y]) {
-						target.add(new int[] {e.x, e.y});
-						kill[e.x][e.y] = true; 
-					}
-				}
-			}
-			//3명의 목표물 삭제
-			int k = 0;
-			for(int[] t : target) {
-				//System.out.printf("<<%d %d>> x : %d, y: %d\n",ivy, k++, t[0], t[1]);
-				for (int i = eList.size()-1; i >= 0; i--) {
-					if(t[0] == eList.get(i)[0] && t[1] == eList.get(i)[1]) {
-						eList.remove(i);
-						cnt++;
-						break;
-					}
-				}
-			}
-			//한 칸 이동
-			for (int i = eList.size()-1; i >= 0; i--) {
-				eList.get(i)[0] += 1;
-				if(eList.get(i)[0] == N) 
-					eList.remove(i);
-			}
-		}
-		return cnt;
-	}
-	
+        N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken()); D = Integer.parseInt(st.nextToken());
 
-	private static class Enemy implements Comparable<Enemy>{
-		int x, y;
-		int dist;
-		
-		Enemy(int x, int y, int dist) {
-			this.x = x;
-			this.y = y;
-			this.dist = dist;
-		}
-		
-		@Override
-		public int compareTo(Enemy o) {
-			if(this.dist == o.dist)
-				return this.y - o.y;
-			return this.dist - o.dist;
-		}
-	}
+        //적 위치 저장
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                if(st.nextToken().equals("1"))
+                    enemies.add(new Pos(i, j));
+            }
+        }
+        //궁수 조합으로 위치 지정
+
+        combi(0, 0);
+
+        System.out.println(maxKillCnt);
+
+    }
+
+    private static void combi(int depth, int start){
+        if(depth == 3){
+            //궁수 위치 세팅됨 -> 시뮬 시작
+            simulation(enemies);
+            return;
+        }
+
+        for (int i = start; i < M; i++) {
+            archers[depth] = new Pos(N, i);
+            combi(depth + 1, i + 1);
+        }
+    }
+
+    private static void simulation(ArrayList<Pos> enemies) {
+        List<Pos> cEnemy = new ArrayList<>();
+        for (Pos p : enemies){
+            cEnemy.add(new Pos(p.x, p.y));
+        }
+        boolean[][] checked = new boolean[N][M];
+        int killCnt = 0;
+        
+        //적군 죽이기
+        while (!cEnemy.isEmpty()) {
+            //궁수 3명을 순회하며 죽일 적군 좌표에 boolean-> true 체크
+
+            for (Pos archer : archers) {
+                int x = -1, y = -1;
+                int minDistance = D;
+                int minY = M;
+                
+                for (Pos e : cEnemy) {
+                    //거리가 D 이하이고, minDist보다 가깝거나
+                    int d = Math.abs(archer.x - e.x) + Math.abs(archer.y - e.y);
+                    
+                    if((minDistance > d || (minDistance == d && e.y < minY))){
+                        x = e.x;
+                        y = e.y;
+                        minY = e.y;
+                        minDistance = d;
+                    }
+                }
+                //checked에 저장
+                
+                if(x != -1 && y != -1)
+                    checked[x][y] = true;
+            }
+            //궁수에 대한 순회가 끝나면 -> cEnemy 배열 돌면서 한칸 전진하고, true인 애들은 삭제해줌
+            for (int i = cEnemy.size()-1; i >= 0; i--) {
+                Pos e = cEnemy.get(i);
+                if(checked[e.x][e.y]){
+                    ++killCnt;
+                    cEnemy.remove(i);
+                    checked[e.x][e.y] = false;
+                    continue;
+                }
+                //살아 남았으면 전진, 아니면 삭제
+                if(e.x + 1 >= N){
+                    cEnemy.remove(i);
+                }else {
+                    cEnemy.get(i).x++;
+                }
+            }
+            
+        }
+        maxKillCnt = Math.max(maxKillCnt, killCnt);
+
+    }
+
+    private static class Pos {
+        int x, y;
+
+        Pos(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
