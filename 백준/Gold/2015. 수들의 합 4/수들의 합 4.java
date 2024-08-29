cea3509cab2e713d05569a1c9cb0f10b26b9358a
glennni <97296543+Ryuuhj@users.noreply.github.com>
2024-08-29 10:50:41 +0900

    [Gold IV] Title: 수들의 합 4, Time: 336 ms, Memory: 32520 KB -BaekjoonHub

---
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		long answer = 0;
+		
+		int[] A = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		HashMap<Integer, Integer> accSum = new HashMap<>();
+		accSum.put(0, 1);
+		int sum = 0, key = 0;
+		for (int i = 0; i < N; i++) {
+			sum += A[i];
+			//sum - x = K, x = sum - K 있는지 확인
+			key = sum - K;
+			if(accSum.containsKey(key))
+				answer += accSum.get(key);
+			
+			accSum.put(sum, accSum.getOrDefault(sum, 0) + 1);
+			
+		}
+		System.out.println(answer);
+
+	}
+
+}
